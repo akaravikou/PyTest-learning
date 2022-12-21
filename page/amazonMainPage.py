4ca9b056e9e2bd04607bddfa488ae8bcda47fe34
Anton Karavikou <akaravikou@solvd.com>
2022-12-21 18:27:32 +0300

    First test was created

---
+++ page/amazonMainPage.py
@@ -0,0 +1,21 @@
+from component.header import Header
+from page.abstractPage import AbstractPage
+
+
+class AmazonMainPage(AbstractPage):
+
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.header = Header(self.driver)
+
+    def click_sign_in_button(self):
+        return self.header.click_sign_in_button()
+
+    def get_user_name(self):
+        return self.header.get_user_name()
+
+    def search_input(self, text):
+        self.header.search_input(text)
+
+    def click_search_button(self):
+        return self.header.click_search_button()
